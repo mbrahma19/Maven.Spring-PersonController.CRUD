01fcc1aa6e4cd6ce7a01c0926dba81705fa76bd1
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-04-01 09:14:31 -0400

    Completed lab

--- src/main/java/io/zipcoder/crudapp/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/PersonController.java
@@ -2,37 +2,44 @@ package io.zipcoder.crudapp.controllers;
 
 import io.zipcoder.crudapp.models.Person;
 import io.zipcoder.crudapp.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class PersonController {
 
+    @Autowired
     private PersonRepository repository;
 
-    public PersonController(PersonRepository repository){this.repository = repository;}
 
-    @PostMapping("/people")
-    public ResponseEntity<Person> createPerson(@RequestBody Person person){
-        return new ResponseEntity<>(repository.save(person), HttpStatus.CREATED);
-    }
+    public PersonController(PersonRepository repository){this.repository = repository;}
 
     @GetMapping("/people/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable Long id){
         return new ResponseEntity<>(repository.findOne(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<List<Person>> getPersonList(){
-        //return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+    @PostMapping("/people")
+    public ResponseEntity<Person> createPerson(@RequestBody Person person){
+        return new ResponseEntity<>(repository.save(person), HttpStatus.CREATED);
+    }
 
-        return null;
+
+    @GetMapping("/people")
+    public ResponseEntity<List<Person>> getPersonList(){
+        List<Person> list = new ArrayList<>();
+        repository.findAll().forEach(s -> list.add(s));
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @PutMapping("/people/{id}")
-    public ResponseEntity<Person> updatePerson(Person person){
+    public ResponseEntity<Person> updatePerson(@RequestBody Person person){
         Long id = person.getId();
         Person originalPerson = repository.findOne(id);
         originalPerson.setFname(person.getFname());
